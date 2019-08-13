d6c24910160a23bf623021f041ec5956275173dc
Theo Butler <theodusbutler@gmail.com>
2019-08-13 17:23:34 +0200

    Remove nightly release date assumptions from tests
    Closes #11

--- test/test.sh
+++ test/test.sh
@@ -6,9 +6,9 @@ set -o nounset
 check_file() {
   expected=$1
 
-  if [ ! -f "$expected" ]; then
+  if [ ! -f "${expected}" ]; then
     printf "\\033[1;91m  ===> error:\\033[0m expected file not found: %s\n" \
-      "$expected"
+      "${expected}"
     exit 1
   fi
 }
@@ -17,10 +17,10 @@ check_output() {
   cmd=$1
   expected=$2
 
-  output=$($cmd | tee /dev/tty)
-  if ! echo "$output" | grep -q "$expected"; then
+  output=$(${cmd} | tee /dev/tty)
+  if ! echo "${output}" | grep -q "${expected}"; then
     printf "\\033[1;91m  ===> error:\\033[0m did not match \"%s\"\n" \
-      "$expected"
+      "${expected}"
     exit 1
   fi
 }
@@ -35,37 +35,44 @@ test_title() {
 ponyup_bin=build/release/ponyup
 version=$(cut -f 1 <VERSION)
 
-prefix="./.pony_test"
-today=$(date +%Y%m%d)
-if uname -s | grep -q "Linux"; then
-  yesterday=$(date +%Y%m%d --date="yesterday")
-else
-  yesterday=$(date -v-1d +%Y%m%d)
-fi
+query_url="https://api.cloudsmith.io/packages/ponylang/nightlies/"
+query="?query=ponyc&page=1&page_size=2"
+response=$(curl --request GET "${query_url}${query}")
+recent_releases=$(echo "${response}" |
+  sed 's/, /\n/g' |
+  awk '/"version":/ {print $2}' |
+  sed 's/"//g')
 
-rm -rf $prefix
+latest=$(echo "${recent_releases}" | head -1)
+echo "latest = ${latest}"
+previous=$(echo "${recent_releases}" | tail -1)
+echo "previous = ${previous}"
+
+prefix="./.pony_test"
+
+rm -rf ${prefix}
 
 test_title "version"
 check_output "${ponyup_bin} version" "ponyup ${version}"
 
 test_title "nightly"
-${ponyup_bin} update nightly --verbose --prefix="$prefix"
-check_file "$prefix/ponyup/nightly-$today/bin/ponyc"
-check_file "$prefix/ponyup/nightly-$today/bin/corral"
-check_file "$prefix/ponyup/nightly-$today/bin/stable"
-check_output "$prefix/ponyup/bin/ponyc --version" "nightly-$today"
+${ponyup_bin} update nightly --verbose --prefix="${prefix}"
+check_file "${prefix}/ponyup/nightly-${latest}/bin/ponyc"
+check_file "${prefix}/ponyup/nightly-${latest}/bin/corral"
+check_file "${prefix}/ponyup/nightly-${latest}/bin/stable"
+check_output "${prefix}/ponyup/bin/ponyc --version" "nightly-${latest}"
 
 test_title "up to date"
-check_output "${ponyup_bin} update -v -p=$prefix nightly-$today" \
-  "nightly-$today is up to date"
-check_file "$prefix/ponyup/nightly-$today/bin/ponyc"
-check_file "$prefix/ponyup/nightly-$today/bin/corral"
-check_file "$prefix/ponyup/nightly-$today/bin/stable"
-check_output "$prefix/ponyup/bin/ponyc --version" "nightly-$today"
+check_output "${ponyup_bin} update -v -p=${prefix} nightly-${latest}" \
+  "nightly-${latest} is up to date"
+check_file "${prefix}/ponyup/nightly-${latest}/bin/ponyc"
+check_file "${prefix}/ponyup/nightly-${latest}/bin/corral"
+check_file "${prefix}/ponyup/nightly-${latest}/bin/stable"
+check_output "${prefix}/ponyup/bin/ponyc --version" "nightly-${latest}"
 
-test_title "nightly (yesterday)"
-${ponyup_bin} -v -p=$prefix update nightly-$yesterday
-check_file "$prefix/ponyup/nightly-$yesterday/bin/ponyc"
-check_file "$prefix/ponyup/nightly-$yesterday/bin/corral"
-check_file "$prefix/ponyup/nightly-$yesterday/bin/stable"
-check_output "$prefix/ponyup/bin/ponyc --version" "nightly-$yesterday"
+test_title "nightly (previous)"
+${ponyup_bin} -v -p=${prefix} update nightly-${previous}
+check_file "${prefix}/ponyup/nightly-${previous}/bin/ponyc"
+check_file "${prefix}/ponyup/nightly-${previous}/bin/corral"
+check_file "${prefix}/ponyup/nightly-${previous}/bin/stable"
+check_output "${prefix}/ponyup/bin/ponyc --version" "nightly-${previous}"
