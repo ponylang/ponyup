027d9a22d98a1960724a00266985424962ce064b
Theo Butler <theodusbutler@gmail.com>
2019-10-14 22:31:01 -0400

    check application versions individually

--- test/test.sh
+++ test/test.sh
@@ -32,6 +32,19 @@ test_title() {
   printf "==============================\\033[0m\n"
 }
 
+latest_versions() {
+  package=$1
+  query_url="https://api.cloudsmith.io/packages/ponylang/nightlies/"
+  query="?query=${package}"
+  if [ "${package}" = "ponyc" ]; then query="${query}%20${libc}"; fi
+  query="${query}%20status:completed&page=1&page_size=2"
+  response=$(curl --request GET "${query_url}${query}")
+  echo "${response}" |
+    sed 's/, /\n/g' |
+    awk '/"version":/ {print $2}' |
+    sed 's/"//g'
+}
+
 ponyup_bin=build/release/ponyup
 version=$(cut -f 1 <VERSION)
 
@@ -40,18 +53,25 @@ echo "triple is ${triple}"
 libc="${triple##*-}"
 echo "libc is ${libc}"
 
-query_url="https://api.cloudsmith.io/packages/ponylang/nightlies/"
-query="?query=ponyc%20${libc}%20status:completed&page=1&page_size=3"
-response=$(curl --request GET "${query_url}${query}")
-recent_releases=$(echo "${response}" |
-  sed 's/, /\n/g' |
-  awk '/"version":/ {print $2}' |
-  sed 's/"//g')
+ponyup_versions=$(latest_versions ponyup)
+latest_ponyup=$(echo "${ponyup_versions}" | head -1)
+previous_ponyup=$(echo "${ponyup_versions}" | tail -1)
+echo "ponyup versions: ${latest_ponyup} ${previous_ponyup}"
 
-latest=$(echo "${recent_releases}" | head -1)
-echo "latest = ${latest}"
-previous=$(echo "${recent_releases}" | tail -1)
-echo "previous = ${previous}"
+ponyc_versions=$(latest_versions ponyc)
+latest_ponyc=$(echo "${ponyc_versions}" | head -1)
+previous_ponyc=$(echo "${ponyc_versions}" | tail -1)
+echo "ponyc versions: ${latest_ponyc} ${previous_ponyc}"
+
+corral_versions=$(latest_versions corral)
+latest_corral=$(echo "${corral_versions}" | head -1)
+previous_corral=$(echo "${corral_versions}" | tail -1)
+echo "corral versions: ${latest_corral} ${previous_corral}"
+
+stable_versions=$(latest_versions stable)
+latest_stable=$(echo "${stable_versions}" | head -1)
+previous_stable=$(echo "${stable_versions}" | tail -1)
+echo "stable versions: ${latest_stable} ${previous_stable}"
 
 prefix="./.pony_test"
 
@@ -62,26 +82,26 @@ check_output "${ponyup_bin} version" "ponyup ${version}"
 
 test_title "nightly"
 ${ponyup_bin} update nightly --verbose --prefix="${prefix}" --libc=${libc}
-check_file "${prefix}/ponyup/nightly-${latest}/bin/ponyc"
-check_file "${prefix}/ponyup/nightly-${latest}/bin/corral"
-check_file "${prefix}/ponyup/nightly-${latest}/bin/stable"
-check_output "${prefix}/ponyup/bin/ponyc --version" "nightly-${latest}"
-check_output "${ponyup_bin} show -v -p=${prefix}" "nightly-${latest}"
+check_file "${prefix}/ponyup/nightly-${latest_ponyc}/bin/ponyc"
+check_file "${prefix}/ponyup/nightly-${latest_corral}/bin/corral"
+check_file "${prefix}/ponyup/nightly-${latest_stable}/bin/stable"
+check_output "${prefix}/ponyup/bin/ponyc --version" "nightly-${latest_ponyc}"
+check_output "${ponyup_bin} show -v -p=${prefix}" "nightly-${latest_ponyup}"
 
 test_title "up to date"
 check_output \
-  "${ponyup_bin} update -v -p=${prefix} --libc=${libc} nightly-${latest}" \
-  "nightly-${latest}-${libc} is up to date"
-check_file "${prefix}/ponyup/nightly-${latest}/bin/ponyc"
-check_file "${prefix}/ponyup/nightly-${latest}/bin/corral"
-check_file "${prefix}/ponyup/nightly-${latest}/bin/stable"
-check_output "${prefix}/ponyup/bin/ponyc --version" "nightly-${latest}"
-check_output "${ponyup_bin} show -v -p=${prefix}" "nightly-${latest}"
+  "${ponyup_bin} update -v -p=${prefix} --libc=${libc} nightly-${latest_ponyc}" \
+  "nightly-${latest_ponyc}-${libc} is up to date"
+check_file "${prefix}/ponyup/nightly-${latest_ponyc}/bin/ponyc"
+check_file "${prefix}/ponyup/nightly-${latest_corral}/bin/corral"
+check_file "${prefix}/ponyup/nightly-${latest_stable}/bin/stable"
+check_output "${prefix}/ponyup/bin/ponyc --version" "nightly-${latest_ponyc}"
+check_output "${ponyup_bin} show -v -p=${prefix}" "nightly-${latest_ponyup}"
 
 test_title "nightly (previous)"
-${ponyup_bin} -v "-p=${prefix}" update "nightly-${previous}" "--libc=${libc}"
-check_file "${prefix}/ponyup/nightly-${previous}/bin/ponyc"
-check_file "${prefix}/ponyup/nightly-${previous}/bin/corral"
-check_file "${prefix}/ponyup/nightly-${previous}/bin/stable"
-check_output "${prefix}/ponyup/bin/ponyc --version" "nightly-${previous}"
-check_output "${ponyup_bin} show -v -p=${prefix}" "nightly-${previous}"
+${ponyup_bin} -v "-p=${prefix}" update "nightly-${previous_ponyc}" "--libc=${libc}"
+check_file "${prefix}/ponyup/nightly-${previous_ponyc}/bin/ponyc"
+check_file "${prefix}/ponyup/nightly-${previous_corral}/bin/corral"
+check_file "${prefix}/ponyup/nightly-${previous_stable}/bin/stable"
+check_output "${prefix}/ponyup/bin/ponyc --version" "nightly-${previous_ponyc}"
+check_output "${ponyup_bin} show -v -p=${prefix}" "nightly-${previous_ponyup}"
