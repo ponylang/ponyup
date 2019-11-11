fea7fd0fb44f57e79bef0634934c291bbff9c019
Sean T. Allen <sean@seantallen.com>
2019-11-11 17:43:55 -0500

    Bring over automation improvements

--- .ci-scripts/release/trigger-release-announcement.bash
+++ .ci-scripts/release/trigger-release-announcement.bash
@@ -15,19 +15,25 @@
 # - git
 
 set -o errexit
-set -o nounset
+
+# Pull in shared configuration specific to this repo
+base=$(dirname "$0")
+source "${base}/config.bash"
 
 # Verify ENV is set up correctly
 # We validate all that need to be set in case, in an absolute emergency,
 # we need to run this by hand. Otherwise the GitHub actions environment should
 # provide all of these if properly configured
-if [[ -z "${GITHUB_ACTOR}" ]]; then
-  echo -e "\e[31mName of the user to make changes to repo as need to be set in GITHUB_ACTOR. Exiting."
-  exit 1
-fi
-
-if [[ -z "${GITHUB_TOKEN}" ]]; then
-  echo -e "\e[31mA personal access token needs to be set in GITHUB_TOKEN. Exiting."
+if [[ -z "${RELEASE_TOKEN}" ]]; then
+  echo -e "\e[31mA personal access token needs to be set in RELEASE_TOKEN."
+  echo -e "\e[31mIt should not be secrets.GITHUB_TOKEN. It has to be a"
+  echo -e "\e[31mpersonal access token otherwise next steps in the release"
+  echo -e "\e[31mprocess WILL NOT trigger."
+  echo -e "\e[31mPersonal access tokens are in the form:"
+  echo -e "\e[31m     USERNAME:TOKEN"
+  echo -e "\e[31mfor example:"
+  echo -e "\e[31m     ponylang-main:1234567890"
+  echo -e "\e[31mExiting.\e[0m"
   exit 1
 fi
 
@@ -36,25 +42,19 @@ if [[ -z "${GITHUB_REF}" ]]; then
   echo -e "\e[31mThe tag should be in the following GitHub specific form:"
   echo -e "\e[31m    /refs/tags/X.Y.Z"
   echo -e "\e[31mwhere X.Y.Z is the version we are announcing"
-  echo -e "\e[31mExiting."
+  echo -e "\e[31mExiting.\e[0m"
   exit 1
 fi
 
-# Set up .netrc file with GitHub credentials
-cat <<- EOF > $HOME/.netrc
-      machine github.com
-      login $GITHUB_ACTOR
-      password $GITHUB_TOKEN
-      machine api.github.com
-      login $GITHUB_ACTOR
-      password $GITHUB_TOKEN
-EOF
-
-chmod 600 $HOME/.netrc
+# no unset variables allowed from here on out
+# allow above so we can display nice error messages for expected unset variables
+set -o nounset
 
 git config --global user.name 'Ponylang Main Bot'
 git config --global user.email 'ponylang.main@gmail.com'
 git config --global push.default simple
+
+PUSH_TO="https://${RELEASE_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
 
 # Extract version from tag reference
 # Tag ref version: "refs/tags/1.0.0"
@@ -62,9 +62,9 @@ git config --global push.default simple
 VERSION="${GITHUB_REF/refs\/tags\//}"
 
 # tag for announcement
-echo -e "\e[34mTagging to kick off release announcement"
+echo -e "\e[34mTagging to kick off release announcement\e[0m"
 git tag "announce-${VERSION}"
 
 # push tag
-echo -e "\e[34mPushing announce-${VERSION} tag"
-git push origin "announce-${VERSION}"
+echo -e "\e[34mPushing announce-${VERSION} tag\e[0m"
+git push ${PUSH_TO} "announce-${VERSION}"
--- .ci-scripts/release/x86-64-unknown-linux-release.bash
+++ .ci-scripts/release/x86-64-unknown-linux-release.bash
@@ -11,21 +11,49 @@
 # - cloudsmith-cli
 # - GNU gzip
 # - GNU make
-# - OpenSSL 0.9.0 conforming SSL
 # - ponyc (musl based version)
 # - GNU tar
 
 set -o errexit
-set -o nounset
+
+# Pull in shared configuration specific to this repo
+base=$(dirname "$0")
+source "${base}/config.bash"
 
 # Verify ENV is set up correctly
 # We validate all that need to be set in case, in an absolute emergency,
 # we need to run this by hand. Otherwise the GitHub actions environment should
 # provide all of these if properly configured
 if [[ -z "${CLOUDSMITH_API_KEY}" ]]; then
-  echo -e "\e[31mCloudsmith API key needs to be set in CLOUDSMITH_API_KEY. Exiting."
+  echo -e "\e[31mCloudsmith API key needs to be set in CLOUDSMITH_API_KEY."
+  echo -e "Exiting.\e[0m"
   exit 1
 fi
+
+if [[ -z "${GITHUB_REPOSITORY}" ]]; then
+  echo -e "\e[31mName of this repository needs to be set in GITHUB_REPOSITORY."
+  echo -e "\e[31mShould be in the form OWNER/REPO, for example:"
+  echo -e "\e[31m     ponylang/ponyup"
+  echo -e "\e[31mExiting.\e[0m"
+  exit 1
+fi
+
+if [[ -z "${APPLICATION_NAME}" ]]; then
+  echo -e "\e[31mAPPLICATION_NAME needs to be set."
+  echo -e "\e[31mExiting.\e[0m"
+  exit 1
+fi
+
+if [[ -z "${APPLICATION_SUMMARY}" ]]; then
+  echo -e "\e[31mAPPLICATION_SUMMARY needs to be set."
+  echo -e "\e[31mIt's a short description of the application that will appear in Cloudsmith."
+  echo -e "\e[31mExiting.\e[0m"
+  exit 1
+fi
+
+# no unset variables allowed from here on out
+# allow above so we can display nice error messages for expected unset variables
+set -o nounset
 
 # Compiler target parameters
 ARCH=x86-64
@@ -42,7 +70,7 @@ BUILD_DIR=${BUILD_PREFIX}/${APPLICATION_VERSION}
 
 # Asset information
 PACKAGE_DIR=$(mktemp -d)
-PACKAGE=ponyup-${TRIPLE}
+PACKAGE=${APPLICATION_NAME}-${TRIPLE}
 
 # Cloudsmith configuration
 CLOUDSMITH_VERSION=$(cat VERSION)
@@ -50,22 +78,22 @@ ASSET_OWNER=ponylang
 ASSET_REPO=releases
 ASSET_PATH=${ASSET_OWNER}/${ASSET_REPO}
 ASSET_FILE=${PACKAGE_DIR}/${PACKAGE}.tar.gz
-ASSET_SUMMARY="The Pony toolchain multiplexer"
-ASSET_DESCRIPTION="https://github.com/ponylang/ponyup"
+ASSET_SUMMARY="${APPLICATION_SUMMARY}"
+ASSET_DESCRIPTION="https://github.com/${GITHUB_REPOSITORY}"
 
-# Build ponyup installation
-echo -e "\e[34mBuilding ponyup..."
+# Build application installation
+echo -e "\e[34mBuilding ${APPLICATION_NAME}...\e[0m"
 make install prefix="${BUILD_DIR}" arch=${ARCH} \
   version="${APPLICATION_VERSION}" static=true linker=bfd
 
 # Package it all up
-echo -e "\e[34mCreating .tar.gz of ponyup..."
+echo -e "\e[34mCreating .tar.gz of ${APPLICATION_NAME}...\e[0m"
 pushd "${BUILD_PREFIX}" || exit 1
-tar -cvzf "${ASSET_FILE}" ./*
+tar -cvzf "${ASSET_FILE}" *
 popd || exit 1
 
 # Ship it off to cloudsmith
-echo -e "\e[34mUploading package to cloudsmith..."
+echo -e "\e[34mUploading package to cloudsmith...\e[0m"
 cloudsmith push raw --version "${CLOUDSMITH_VERSION}" \
   --api-key "${CLOUDSMITH_API_KEY}" --summary "${ASSET_SUMMARY}" \
   --description "${ASSET_DESCRIPTION}" ${ASSET_PATH} "${ASSET_FILE}"
