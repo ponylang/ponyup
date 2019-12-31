e48ee88af32e46e8fe031a043d8ff1310b40a070
Sean T. Allen <sean@seantallen.com>
2019-12-30 20:12:02 -0500

    Add macOS specific ponyup install location to $PATH

--- .ci-scripts/test-bootstrap.sh
+++ .ci-scripts/test-bootstrap.sh
@@ -2,7 +2,7 @@
 
 cat ponyup-init.sh | sh -s
 
-export PATH=$HOME/.local/share/ponyup/bin:$PATH
+export PATH=$HOME/.local/share/ponyup/bin:"/Users/runner/Library/Application Support/ponyup/bin":$PATH
 ponyup update ponyc nightly
 ponyup update changelog-tool nightly
 ponyup update corral nightly
