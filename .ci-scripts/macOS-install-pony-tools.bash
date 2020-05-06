ecc0fcb1506a709e7489b9a117587bfd950539f7
Sean T. Allen <sean@seantallen.com>
2020-05-05 20:23:18 -0400

    Fix macOS pony tool installation
    Missing libressl is causing ponyup to fail

--- .ci-scripts/macOS-install-pony-tools.bash
+++ .ci-scripts/macOS-install-pony-tools.bash
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+#
+# Libresll is required for ponyup
+#
+
+brew install libressl
+
+#
+# Install ponyup and other tools
+#
+
 curl --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/ponylang/ponyup/latest-release/ponyup-init.sh | sh
 
 export PATH="$HOME/.local/share/ponyup/bin/:$PATH"
