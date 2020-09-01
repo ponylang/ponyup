e1f61cb50044ddca36be5556ae7daec7e08e695f
Sean T. Allen <sean@seantallen.com>
2020-09-01 17:13:33 -0400

    Add CentOS 8 bootstrap testing
    Closes #164

---
+++ .ci-dockerfiles/centos08-bootstrap-tester/build-and-push.bash
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+
+#
+# *** You should already be logged in to DockerHub when you run this ***
+#
+
+TODAY=$(date +%Y%m%d)
+DOCKERFILE_DIR="$(dirname "$0")"
+
+docker build --pull \
+  -t "ponylang/ponyup-ci-centos8-bootstrap-tester:${TODAY}" \
+  "${DOCKERFILE_DIR}"
+docker push "ponylang/ponyup-ci-centos8-bootstrap-tester:${TODAY}"
