02bf27ce8789b56e68565060263c5f7ff0ae70f5
Sean T. Allen <sean@seantallen.com>
2024-04-25 22:58:18 +0000

    Add Ubuntu 24.04 support
    Closes #302

---
+++ .ci-dockerfiles/ubuntu24.04-bootstrap-tester/build-and-push.bash
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+
+#
+# *** You should already be logged in to GHCR when you run this ***
+#
+
+TODAY=$(date +%Y%m%d)
+DOCKERFILE_DIR="$(dirname "$0")"
+DOCKER_TAG="ghcr.io/ponylang/ponyup-ci-ubuntu24.04-bootstrap-tester:${TODAY}"
+
+docker build --pull -t "${DOCKER_TAG}" "${DOCKERFILE_DIR}"
+docker push "${DOCKER_TAG}"
